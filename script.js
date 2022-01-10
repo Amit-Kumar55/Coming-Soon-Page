d9abf145e06617aee3b8c1d77a732e1702051e65
Amit <amitkumar.ak12377@gmail.com>
2022-01-10 17:44:43 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const form = document.getElementById("form")
+
+const email = document.getElementById("email")
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const emailval = email.value
+
+    // Check if it is a valid email or not
+    if(!validateEmail(emailval)){
+        form.classList.add('error');
+        form.classList.remove('success');
+    }
+    else{
+        form.classList.remove('error')
+        form.classList.add('success')
+    }
+});
+
+function validateEmail(email){
+    var re =   
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        return re.test(String(email).toLowerCase());
+    }
